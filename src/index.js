09460a6e71acfd7083a9e849bc8415035d1739bf
Emilio Villuendas Zambrana <emilio.villuendas@adevinta.com>
2021-04-13 18:17:58 +0200

    add prop

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 /* eslint-disable react/forbid-prop-types */
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { useState } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
-import SliderData from './SliderData'
 
 import './scss/index.scss'
 
 const baseClass = 'slider'
 
-const ImageSlider = ({ slides = SliderData }) => {
+const ImageSlider = ({ onCurrentChange, slides }) => {
   const [current, setCurrent] = useState(0)
-  const { length } = slides
+  const length = slides?.length
+
+  useEffect(() => {
+    onCurrentChange(current)
+  }, [current])
+
+  const slideImages = useMemo(() => slides, [slides])
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null
+  }
 
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1)
@@ -21,15 +30,11 @@ const ImageSlider = ({ slides = SliderData }) => {
     setCurrent(current === 0 ? length - 1 : current - 1)
   }
 
-  if (!Array.isArray(slides) || slides.length <= 0) {
-    return null
-  }
-
   return (
     <section className={baseClass}>
       <FaArrowAltCircleLeft className={`${baseClass}-arrowLeft`} onClick={prevSlide} />
       <FaArrowAltCircleRight className={`${baseClass}-arrowRight`} onClick={nextSlide} />
-      {SliderData.map((slide, index) => (
+      {slideImages.map((slide, index) => (
         <div
           className={index === current ? `${baseClass}-slide active` : `${baseClass}-slide`}
           key={slide.id}
@@ -47,6 +52,7 @@ const ImageSlider = ({ slides = SliderData }) => {
 export default ImageSlider
 
 ImageSlider.propTypes = {
+  onCurrentChange: PropTypes.func.isRequired,
   slides: PropTypes.object.isRequired,
 
 }
